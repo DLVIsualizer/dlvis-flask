c11f7ee68b4435edc8d3fdad04ce56ec9ecc638a
ByoungWook Park <qkrquddnr99@gmail.com>
2018-04-15 10:46:53 +0900

    layer.py -> function added : setUnitsDependsOnType

--- dlv/filter.py
+++ dlv/filter.py
@@ -1,5 +1,11 @@
+import numpy as np
+
 class Filter:
-    def __init__(self, filter):
+    def __init__(self, filter:np.ndarray):
+        """
+
+        :param filter: [Height,Width,Depth] Array
+        """
         self._filter = filter
         self._neurons = []
         self._featureMap = []
--- dlv/layer.py
+++ dlv/layer.py
@@ -1,4 +1,6 @@
 import keras
+import tensorflow as tf
+import numpy as np
 
 """
 Resnet50 layers classes
@@ -33,9 +35,18 @@ class Layer:
 		self._layer = k_layer
 		self._layerType = k_layerName
 		self._units = []
+		self.setUnitsDependsOnType()
 	
-	def setUnits(self):
-		pass
+	def setUnitsDependsOnType(self):
+		filters = []
+		if(self._layerType == 'Conv2D'):
+			weights= self._layer.get_weights()[0]
+			biases = self._layer.get_weights()[1]
+			for i in range(weights.shape[3]):
+				filters += [weights[:,:,:,i]]
+			print(filters)
+				
+		
 	
 	def getUnits(self):
 		pass
